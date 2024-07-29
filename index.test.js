91defabd0415ff7b09fc8a2e4a10aaa965f9d558
Amit M <amit@itsallsavvy.com>
2024-07-30 01:16:17 +0530

    add first simple test

--- index.test.js
+++ index.test.js
@@ -1,1 +1,7 @@
-test('should test nothing', () => {  })
+const add = (stringValue) => {
+  return parseInt(stringValue);
+};
+
+test("should check if add('1') returns 1", () => {
+  expect(add("1")).toBe(1);
+});
