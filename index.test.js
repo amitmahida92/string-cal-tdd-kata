0ebec8d75547cae3dd1bf2983aabb83c9f672d80
Amit M <amit@itsallsavvy.com>
2024-07-30 01:18:48 +0530

    add second test with two numbers in string

--- index.test.js
+++ index.test.js
@@ -1,7 +1,18 @@
 const add = (stringValue) => {
+  if (stringValue === "") {
+    return 0;
+  }
+  if (stringValue.includes(",")) {
+    const numbers = stringValue.split(",");
+    return parseInt(numbers[0]) + parseInt(numbers[1]);
+  }
   return parseInt(stringValue);
 };
 
 test("should check if add('1') returns 1", () => {
   expect(add("1")).toBe(1);
+});
+
+test("should check if add('1,2') returns 3", () => {
+  expect(add("1,2")).toBe(3);
 });
