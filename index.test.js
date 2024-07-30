b30f8e814762b7e58a3cff0aebd65a3c61153341
Amit M <amit@itsallsavvy.com>
2024-07-30 21:41:52 +0530

    add test to check negative numbers with delimer and modified add to pass that test

--- index.test.js
+++ index.test.js
@@ -22,13 +22,14 @@ const add = (stringValue) => {
         if (parseInt(number) >= 0) {
           total += parseInt(number);
         } else {
-          negativeNumbers += `${number}`;
+          negativeNumbers += `${number}, `;
         }
       }
     }
   }
   if (negativeNumbers?.length || parseInt(stringValue) < 0) {
-    throw new Error(`negative numbers not allowed ${negativeNumbers || stringValue}`);
+    negativeNumbers = negativeNumbers.slice(0, -2) || stringValue;
+    throw new Error(`negative numbers not allowed ${negativeNumbers}`);
   }
   return total || parseInt(stringValue);
 };
@@ -65,6 +66,10 @@ test("should throw an exception if a negative number is inputed as add('-1')", (
   expect(() => add("-1")).toThrow("negative numbers not allowed -1");
 });
 
-test("should throw an exception if more than one negative number is inputed as add('-1, -2, -3') without delimiter", () => {
-  expect(() => add("-1, -2, -3")).toThrow("negative numbers not allowed -1 -2 -3");
+test("should throw an exception if more than one negative number is inputed as add('-1,-2,-3') without delimiter", () => {
+  expect(() => add("-1,-2,-3")).toThrow("negative numbers not allowed -1, -2, -3");
+});
+
+test("should throw an exception if more than one negative number is inputed as add('//;\n-1;-2;-3') with delimiter", () => {
+  expect(() => add("//;\n-1;-2;-3")).toThrow("negative numbers not allowed -1, -2, -3");
 });
